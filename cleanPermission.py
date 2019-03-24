0dbc1c9b5396b8d3a254834780401f08617b4161
tpsoe <tpsoe@cpp.edu>
2019-03-24 02:36:34 -0700

    clean the permissionTextFile with cleanpermission.py

--- cleanPermission.py
+++ cleanPermission.py
@@ -7,11 +7,11 @@ files = os.listdir(path)
 for name in files:
 	if name.endswith((".txt","_files")):
 		file = name[0:len(name)-4]
-		f = open(name,"r+")
-		
-		for x in f:
-			y = x.replace("uses-permission: name='","")
-			z = y.replace("'","")
-			
-			f.write(z)
-		f.close()
+		with open(name,"r+") as f:
+			for x in f:
+				y=x.replace("uses-permission: name='","")
+				z=y.replace("'","")
+				a=z.replace("uses-permission-sdk-23: name=","")
+				open(name,'w').close()
+			#	wr.write(z)
+				f.write(a)
